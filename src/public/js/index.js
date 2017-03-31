e3759637b4fb44c4355f1fb873d629ab9814a950
webxuhan <xuha_715@163.com>
2017-03-31 14:46:24 +0800

    前台首页

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,34 +1,13 @@
+// 医院挂号系统主要js
 $(function(){
-	// 用户退出
-	$(".userout").on("mouseover",function(){
-		$(this).children("li").children("ul").css("display","block");
+	$(".index_top_nav li").on("mouseover",function(){
+		// console.log(1);
+		$(this).addClass("navCurrent");
+		$(".index_top_showvalue").css("display","inline-block")
 	});
-	$(".userout").on("mouseout",function(){
-		$(this).children("li").children("ul").css("display","none");
+	$(".index_top_nav li").on("mouseout",function(){
+		// console.log(2);
+		$(this).removeClass("navCurrent");
+		$(".index_top_showvalue").css("display","none");
 	});
-	// 一级分类列表点击
-	$(".main-nav>li>ul").slideUp();
-	$(".main-nav>li").off("click");
-	$(".main-nav>li").on("click",function(){	
-		if($(this).children("ul").css("display") =="none"){
-			$(".main-nav>li").removeClass("current");
-			$(this).addClass("current");
-			$(".main-nav>li>ul").slideUp(500);
-			$(this).children("ul").stop().slideDown(500);
-		}
-	});
-	// $(".main-nav>li>ul").eq(0).slideDown(300);
-	//二级分类列表点击
-	$(".main-nav>li>ul>li").off("click");
-	$(".main-nav>li>ul>li").on("click",function(){
-		var s = $(this).children().children().eq(1).html();
-		if(!$(this).hasClass("line-current")&&(s != undefined)){
-			$(".main-nav>li>ul>li>a>span").removeClass("line-current");		
-			$(this).children().children(".line").addClass("line-current");
-			var url = $(this).attr("url");
-			$(".main-content").load(url);
-		}
-	});
-	 $(".main-nav>li").eq(0).trigger("click");	
-	 $(".main-nav>li>ul>li").eq(1).trigger("click"); 
-})
+});
